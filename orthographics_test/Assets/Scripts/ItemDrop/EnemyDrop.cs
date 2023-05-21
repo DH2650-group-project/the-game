cae3fa4e7852fa941479a08ab9157afae0bad389
Jesper Lidbaum <jesper.r.00@gmail.com>
2023-05-21 13:18:03 +0200

    Added individual drop chances

--- orthographics_test/Assets/Scripts/ItemDrop/EnemyDrop.cs
+++ orthographics_test/Assets/Scripts/ItemDrop/EnemyDrop.cs
@@ -1,26 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDrop : MonoBehaviour
 {
+    [Serializable]
+    public struct DropPair
+    {
+        public EquippableItem_SO item;
+        public float dropChance;
+        public DropPair(EquippableItem_SO item, float dropChance)
+        {
+            this.item = item;
+            this.dropChance = dropChance;
+        }
+    }
 
     [SerializeField]
-    EquippableItem_SO[] loot_table;
+    DropPair[] loot_table;
 
     [SerializeField]
     GameObject itemDrop;
 
-    [SerializeField]
-    float dropChance = 0.1f;
-
     private void OnDestroy()
     {
-        if(Random.Range(0f, 1f) <= dropChance)
+
+        float dropRoll = UnityEngine.Random.Range(0f, 1f);
+        int lowest = -1;
+
+        for (int i = 0; i < loot_table.Length; i++) 
         {
-            int randomItem = Random.Range(0, loot_table.Length);
-            GameObject thisDrop = Instantiate(itemDrop, transform.position, Quaternion.identity);
-            thisDrop.GetComponent<ItemPickUp>().item = loot_table[randomItem];
+            if(dropRoll <= loot_table[i].dropChance)
+            {
+                if(lowest == -1)
+                {
+                    lowest = i;
+                }
+                else if (loot_table[i].dropChance < loot_table[lowest].dropChance)
+                {
+                    lowest = i;
+                }
+            }
+        }
+
+        if(lowest != -1)
+        {
+            GameObject drop = Instantiate(itemDrop, transform.position, Quaternion.identity);
+            drop.GetComponent<ItemPickUp>().item = loot_table[lowest].item;
+        }
+        else
+        {
+            Debug.Log("No item dropped");
         }
     }
 
