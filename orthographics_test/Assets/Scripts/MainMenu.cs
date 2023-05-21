25955034cc87435f443cde2c5672938d2c9bd23a
dasberg <dasberg@kth.se>
2023-05-21 15:13:34 +0200

    Fixed main menu

--- orthographics_test/Assets/Scripts/MainMenu.cs
+++ orthographics_test/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // load scene "CyberScene0"
+        SceneManager.LoadScene("CyberScene0");
     }
 }
